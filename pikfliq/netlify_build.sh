9caf91e876f1b76b6c50bca33ab2f878e71cff8a
User5678953 <kendynwebster@gmail.com>
2025-05-31 12:18:54 -0500

    Update shell

--- pikfliq/netlify_build.sh
+++ pikfliq/netlify_build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "🔧 Installing Flutter..."
+echo "📦 Installing Flutter SDK..."
 git clone https://github.com/flutter/flutter.git
 export PATH="$PATH:$(pwd)/flutter/bin"
 
 flutter doctor
 
-echo "🛠 Building Flutter Web..."
+echo "🏗 Building Flutter Web..."
 flutter build web --web-renderer html
 
-echo "🔑 Injecting TMDB API key..."
+echo "🔑 Injecting TMDB API Key..."
 sed -i 's/REPLACE_TMDB_API_KEY/'"$TMDB_API_KEY"'/g' build/web/index.html
